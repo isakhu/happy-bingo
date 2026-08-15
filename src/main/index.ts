413ef172cba4ea9b4137d1995e0537e5fa50adbc
Yishak Tule <yishakhak@gmail.com>
2026-08-15 21:23:27 +0300

    Add Electron main process

---
+++ src/main/index.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'node:path'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 700,
+    title: 'Happy Bingo',
+    backgroundColor: '#0b1020',
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  if (process.env.ELECTRON_RENDERER_URL) {
+    win.loadURL(process.env.ELECTRON_RENDERER_URL)
+  } else {
+    win.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
