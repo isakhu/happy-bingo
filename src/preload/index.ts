cb907d58402a7c715fb219e06f44368ad9de96d6
Yishak Tule <yishakhak@gmail.com>
2026-08-16 13:45:38 +0300

    Expose fresh cartella PDF generator to manager UI

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,7 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('happyBingo', {
   appName: 'Happy Bingo',
   version: '0.1.0',
+  generateCardsPdf: () => ipcRenderer.invoke('generate-cards-pdf'),
 })
