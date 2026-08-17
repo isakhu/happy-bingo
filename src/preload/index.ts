ebf26ce8967adf01acafa78cc4884598a09d1533
Yishak Tule <yishakhak@gmail.com>
2026-08-17 23:22:27 +0300

    Expose secure licensing and manager authorization APIs

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -3,6 +3,9 @@ import { contextBridge, ipcRenderer } from 'electron'
 contextBridge.exposeInMainWorld('happyBingo', {
   appName: 'Happy Bingo',
   version: '0.1.0',
+  getLicenseInfo: () => ipcRenderer.invoke('license-info'),
+  activateLicense: (key: string) => ipcRenderer.invoke('activate-license', key),
+  managerPassword: (password: string) => ipcRenderer.invoke('manager-password', password),
   generateCardsPdf: (cards?: unknown[], setId?: string) => ipcRenderer.invoke('generate-cards-pdf', cards, setId),
   playVoice: (file: string) => ipcRenderer.invoke('play-voice', file),
   voiceHealth: () => ipcRenderer.invoke('voice-health'),
