919f352510ec69f486b2618106fc95a5c4448552
Yishak Tule <yishakhak@gmail.com>
2026-08-17 23:19:02 +0300

    Pass the saved Cartella set into PDF printing

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -3,7 +3,7 @@ import { contextBridge, ipcRenderer } from 'electron'
 contextBridge.exposeInMainWorld('happyBingo', {
   appName: 'Happy Bingo',
   version: '0.1.0',
-  generateCardsPdf: () => ipcRenderer.invoke('generate-cards-pdf'),
+  generateCardsPdf: (cards?: unknown[], setId?: string) => ipcRenderer.invoke('generate-cards-pdf', cards, setId),
   playVoice: (file: string) => ipcRenderer.invoke('play-voice', file),
   voiceHealth: () => ipcRenderer.invoke('voice-health'),
 })
