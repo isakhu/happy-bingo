05a226744e1e7e15fc6a9b64e5db2cdb9d912787
Yishak Tule <yishakhak@gmail.com>
2026-08-18 01:08:23 +0300

    Expose customer package builder in Card Setup

--- card-setup/preload.cjs
+++ card-setup/preload.cjs
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('cardSetup', {
   saveHbc: payload => ipcRenderer.invoke('save-hbc', payload),
   installHbc: payload => ipcRenderer.invoke('install-hbc', payload),
-  openHbc: () => ipcRenderer.invoke('open-hbc')
+  openHbc: () => ipcRenderer.invoke('open-hbc'),
+  buildCustomerPackage: payload => ipcRenderer.invoke('build-customer-package', payload)
 })
