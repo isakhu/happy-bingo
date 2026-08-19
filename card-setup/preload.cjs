ae6f69eedabbf6c3f065adf929e9c736356c89cd
Yishak Tule <yishakhak@gmail.com>
2026-08-19 14:41:37 +0300

    Remove Card Setup app

--- card-setup/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-contextBridge.exposeInMainWorld('cardSetup', {
-  saveHbc: payload => ipcRenderer.invoke('save-hbc', payload),
-  installHbc: payload => ipcRenderer.invoke('install-hbc', payload),
-  openHbc: () => ipcRenderer.invoke('open-hbc'),
-  buildCustomerPackage: payload => ipcRenderer.invoke('build-customer-package', payload)
-})
