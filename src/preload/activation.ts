26938c8ab7c1100bd4ced58bc32e039e2193d173
Yishak Tule <yishakhak@gmail.com>
2026-08-19 13:40:08 +0300

    Remove duplicate activation preload entry

--- src/preload/activation.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('happyBingoActivation', {
-  submit: (key: string) => ipcRenderer.invoke('submit-license', key),
-})
