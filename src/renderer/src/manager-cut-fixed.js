f44917aecdfcdf8d40575b74f6b4d6b9e3a564da
Yishak Tule <yishakhak@gmail.com>
2026-08-22 19:05:52 +0300

    chore: lock manager cut to fixed 20 percent

---
+++ src/renderer/src/manager-cut-fixed.js
@@ -0,0 +1,6 @@
+// Fixed manager cut: 20% for every game.
+// This module intentionally contains no DOM or React observers.
+const FIXED_MANAGER_CUT = '20'
+localStorage.setItem('happy-bingo-cut', FIXED_MANAGER_CUT)
+
+export const MANAGER_CUT_PERCENT = 20
